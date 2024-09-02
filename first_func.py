31b20b30360b5080989c7bdd013aae58f8da39ba
yuyu <swharriet2012@gmail.com>
2024-09-02 13:14:40 +1000

    original

---
+++ first_func.py
@@ -0,0 +1,8 @@
+# Define a function named make_noise  that prints "THE CROWD GOES WILD" (make sure you spell it exactly the same).  Execute it once at the bottom of your file.
+
+#Define your make_noise function below
+def make_noise():
+    print("THE CROWD GOES WILD")
+
+#Then, call make_noise once:
+make_noise()
